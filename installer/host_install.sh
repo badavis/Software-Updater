6df48cf1b334614ad788f4cb9277f44625cc2614
jpere039 <jpere039@ucr.edu>
2015-02-26 00:48:44 +0000

    Script update. Ending for today

--- installer/host_install.sh
+++ installer/host_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#---------------------------------------
+#   HOST INSTALLATION
+#---------------------------------------
+
 #Install IP Mapping software for Diagnostics
 yum -y install nmap
 
@@ -7,8 +11,21 @@ yum -y install nmap
 yum -y install httpd
 
 #Install web interface to appropriate location
-cp -R ./host_files/web_interface/* /var/www/html
+cp -R ./host/web_interface/* /var/www/html
 
 #Start apache and configure to run automatically at boot
 service httpd start
 chkconfig httpd on
+
+#---------------------------------------
+#   Create client files and installer
+#---------------------------------------
+
+#Create installer and storage directories
+mkdir ./client_installer
+mkdir ./client_installer/client
+mkdir ./client_installer/client_scripts
+
+#Populate client installer
+cp ./host/client_gen/client_install.sh ./client_installer
+cp ./host/client_gen/client_scripts/* ./client_installer/client/client_scripts
