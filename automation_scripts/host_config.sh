ee7936b771d072bf7373f8bccaa9da547d807504
jpere039 <jpere039@ucr.edu>
2015-02-26 00:22:43 +0000

    Working automated web install

--- automation_scripts/host_config.sh
+++ automation_scripts/host_config.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+#Install IP Mapping software for Diagnostics
 yum -y install nmap
+
+#Install Apache web server
 yum -y install httpd
 
-cp -R ~/automation_scripts/host_install/web_interface/* /var/www/html
+#Install web interface to appropriate location
+cp -R ./host_files/web_interface/* /var/www/html
+
+#Start apache and configure to run automatically at boot
+service httpd start
+chkconfig httpd on
