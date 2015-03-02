ac413c918984987c8511eec3339393ebcf1e7e69
Benjamin Davis <bjamin.davis@gmail.com>
2015-03-02 05:23:10 +0000

    Added Javascript

---
+++ log_scripts/displayupdates.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $.getJSON('../log_scripts/updatelog.json', function(data) {
+            var output="<ul>";
+            
+                output+="<li>" + data.hostname + "</li>";
+    
+            output+="</ul>";
+            document.getElementById("demo").innerHTML=output;
+      });    
+});
+    
