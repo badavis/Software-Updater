0c134f497586c4a273c8dab6ae42e9cb6bf7e9f4
zpzim <zpzimmerman@outlook.com>
2015-03-02 06:48:29 -0800

    Update Website

--- host_installer/host/web_interface/parsejson.js
+++ host_installer/host/web_interface/parsejson.js
@@ -1,10 +1,30 @@
 $( document ).ready(function() {
-    $.getJSON('../log_scripts/updatelog.json', function(data) {
-            var output="<ul>";
+    $.getJSON('updatelog.json', function(data) {
+	
+		var output = 	 "<tr> <td>List of Machines </td> <td>Up To Date </td> <td>Online State</td> </tr>"
+	
+	
+            output +="<tr>";
             
-                output+="<li>" + data.hostname + "</li>";
+                output+="<td>" + data.hostname + "</td>";
+
+                var key, count = 0;
+				for(key in data.Packages) {
+					if(data.Packages.hasOwnProperty(key)) {
+						count++;
+					}
+
+				}
+				if(!count){
+					output+="<td>" + "YES" + "</td>";
+					
+				}
+				else{
+					output+="<td>" + "NO" + "</td>";
+				}
+				output+="<td>" + "BRUH" + "</td>";
     
-            output+="</ul>";
+            output+="</tr>";
             document.getElementById("demo").innerHTML=output;
       });    
 });
