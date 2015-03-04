a11d100a369a400b94d25c773d325021764b7669
zpzim <zpzimmerman@outlook.com>
2015-03-03 17:46:45 -0800

    Update UpdateAll.sh

--- host_installer/host/client_gen/client/program_files/UpdateAll.sh
+++ host_installer/host/client_gen/client/program_files/UpdateAll.sh
@@ -2,6 +2,6 @@
 host="10.0.2.7"
 yum update -y 1>OUT 2>ERR
 client_id=`hostname`
-cat ERR | ssh root@host -i ~/.ssh/$client_id "cat >> /etc/sccm/ERRLOG.log"
-cat OUT | ssh root@host -i ~/.ssh/$client_id "cat >> /etc/sccm/SYSLOG.log"
+cat ERR | ssh root@host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/ERRLOG.log"
+cat OUT | ssh root@host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/SYSLOG.log"
 rm -f ERR OUT
