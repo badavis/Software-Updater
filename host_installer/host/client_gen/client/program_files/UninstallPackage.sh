3cf18f7965d364a4f36c3572fce686c6c3624b83
zpzim <zpzimmerman@outlook.com>
2015-03-03 17:46:18 -0800

    Update UninstallPackage.sh

--- host_installer/host/client_gen/client/program_files/UninstallPackage.sh
+++ host_installer/host/client_gen/client/program_files/UninstallPackage.sh
@@ -2,6 +2,6 @@
 host="10.0.2.7"
 client_id=`hostname`
 yum remove -y $1 1>OUT 2>ERR
-cat ERR | ssh root@$host -i ~/.ssh/$client_id “cat >> /etc/sccm/logs/ERRLOG.log”
-cat OUT | ssh root@$host -i ~/.ssh/$client_id “cat >> /etc/sccm/logs/OUTPUT.log”
+cat ERR | ssh root@$host -i ~/.ssh/$client_id “cat >> /var/www/html/sccm/ERRLOG.log”
+cat OUT | ssh root@$host -i ~/.ssh/$client_id “cat >> /var/www/html/sccm/OUTPUT.log”
 rm -f ERR OUT
