d1e7821d7f673ea7d70db03d67a95422e313d46f
jpere039 <jpere039@ucr.edu>
2015-03-05 00:06:08 +0000

    Edited client script output for readability

--- host_installer/host/client_gen/client/program_files/UpdateAll.sh
+++ host_installer/host/client_gen/client/program_files/UpdateAll.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 host="GENERATED_HOST_ID"
-yum update -y 1>OUT 2>ERR
 client_id=`hostname`
+
+echo "------------------------------------" >> OUT
+echo "      UPDATING ALL PACKAGES         " >> OUT
+echo "------------------------------------" >> OUT
+
+yum update -y 1>OUT 2>ERR
+
+echo "------------------------------------" >> OUT
+echo "    DONE UPDATING ALL PACKAGES      " >> OUT
+echo "------------------------------------" >> OUT
+
 cat ERR | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/ERRLOG.log"
 cat OUT | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/SYSLOG.log"
 rm -f ERR OUT
--- host_installer/host/client_gen/client/program_files/UpdatePackage.sh
+++ host_installer/host/client_gen/client/program_files/UpdatePackage.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 host="GENERATED_HOST_ID"
 client_id=`hostname`
+
+echo "------------------------------------" >> OUT
+echo "      UPDATING $1                   " >> OUT
+echo "------------------------------------" >> OUT
+
 yum update -y $1 1>OUT 2>ERR
+
+echo "------------------------------------" >> OUT
+echo "    DONE UPDATING $1                " >> OUT
+echo "------------------------------------" >> OUT
+
 cat ERR | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/ERRLOG.log"
 cat OUT | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/SYSLOG.log"
 rm -f ERR OUT
