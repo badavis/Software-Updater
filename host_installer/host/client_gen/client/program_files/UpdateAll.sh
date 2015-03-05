8ea4579ee0cfdeb304c9def2fb0a4dc36b9abb44
jpere039 <jpere039@ucr.edu>
2015-03-05 00:51:03 +0000

    Improved client scripts for readability

--- host_installer/host/client_gen/client/program_files/UpdateAll.sh
+++ host_installer/host/client_gen/client/program_files/UpdateAll.sh
@@ -2,15 +2,15 @@
 host="GENERATED_HOST_ID"
 client_id=`hostname`
 
-echo "------------------------------------" >> OUT
-echo "      UPDATING ALL PACKAGES         " >> OUT
-echo "------------------------------------" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+echo "<font color="red">UPDATING ALL PACKAGES</font>" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
 
 yum update -y 1>OUT 2>ERR
 
-echo "------------------------------------" >> OUT
-echo "    DONE UPDATING ALL PACKAGES      " >> OUT
-echo "------------------------------------" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+echo "<font color="red">DONE UPDATING ALL PACKAGES</font>" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
 
 cat ERR | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/ERRLOG.log"
 cat OUT | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/SYSLOG.log"
--- host_installer/host/client_gen/client/program_files/UpdatePackage.sh
+++ host_installer/host/client_gen/client/program_files/UpdatePackage.sh
@@ -2,15 +2,16 @@
 host="GENERATED_HOST_ID"
 client_id=`hostname`
 
-echo "------------------------------------" >> OUT
-echo "      UPDATING $1                   " >> OUT
-echo "------------------------------------" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+echo "<font color="red">UPDATING PACKAGE: $1</font>" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
 
-yum update -y $1 1>OUT 2>ERR
+yum update -y $1 1>>OUT 2>>ERR
 
-echo "------------------------------------" >> OUT
-echo "    DONE UPDATING $1                " >> OUT
-echo "------------------------------------" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+echo "<font color="red">DONE UPDATING PACKAGE: $1</font>" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+
 
 cat ERR | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/ERRLOG.log"
 cat OUT | ssh root@$host -i ~/.ssh/$client_id "cat >> /var/www/html/sccm/SYSLOG.log"
