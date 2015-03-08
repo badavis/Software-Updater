4d8cde7f819eddf72926c103b8dd7ca46e8d3d35
jpere039 <jpere039@ucr.edu>
2015-03-08 23:41:46 +0000

    Updated Script Output

--- host_installer/host/client_gen/client/program_files/UninstallPackage.sh
+++ host_installer/host/client_gen/client/program_files/UninstallPackage.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 host="GENERATED_HOST_ID"
 client_id=`hostname`
-yum remove -y $1 1>OUT 2>ERR
+
+echo "--------------------------------------------------------------------------" >> OUT
+echo "<font color="red">REMOVING PACKAGE: $1</font>" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+
+yum remove -y $1 1>>OUT 2>>ERR
+
+echo "--------------------------------------------------------------------------" >> OUT
+echo "<font color="red">DONE REMOVING PACKAGE: $1</font>" >> OUT
+echo "--------------------------------------------------------------------------" >> OUT
+
 cat ERR | ssh root@$host -i ~/.ssh/$client_id “cat >> /var/www/html/sccm/ERRLOG.log”
 cat OUT | ssh root@$host -i ~/.ssh/$client_id “cat >> /var/www/html/sccm/OUTPUT.log”
 rm -f ERR OUT
