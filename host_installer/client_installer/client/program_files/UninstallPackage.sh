6e307ea7727d408f00a63571821ee669e652b946
zpzim <zpzimmerman@outlook.com>
2015-03-09 00:11:29 -0700

    fixed stuff

---
+++ host_installer/client_installer/client/program_files/UninstallPackage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+host="farmmaster"
+client_id=`hostname`
+yum remove -y $1 1>OUT 2>ERR
+cat ERR | ssh root@$host -i ~/.ssh/$client_id “cat >> /var/www/html/sccm/ERRLOG.log”
+cat OUT | ssh root@$host -i ~/.ssh/$client_id “cat >> /var/www/html/sccm/OUTPUT.log”
+rm -f ERR OUT
