83a658cccb43695f703897bae7e1f2406a976f6a
zpzim <zpzimmerman@outlook.com>
2015-03-09 12:21:38 -0700

    Update findOld.sh

--- host_installer/host/client_gen/client/program_files/findOld.sh
+++ host_installer/host/client_gen/client/program_files/findOld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Checks all installed packages, checks all possible updates, list them
-rm -f availUpdates
+rm -f /etc/sccm/availUpdates
 for packages in `yum list updates -q | awk ' NR>1 {print $1}'`; do
-	yum list ${packages} -q >> availUpdates
+	yum list ${packages} -q >> /etc/sccm/availUpdates
 done
